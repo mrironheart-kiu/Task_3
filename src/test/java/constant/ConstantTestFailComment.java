a8d00fb4e11e25ca764b593458811abe00d8ec88
Kostin Ivan <mrironheart@gmail.com>
2026-03-12 20:37:29 +0300

    Добавил класс констант с ошибками сравнений Assert

---
+++ src/test/java/constant/ConstantTestFailComment.java
@@ -0,0 +1,8 @@
+package constant;
+
+/**
+ * Класс для хранения переменных, содержащих тексты ошибок для провалленных тестов
+ */
+public class ConstantTestFailComment {
+    public static final String DEFAULT_ERROR_COMMENT = "Фактический результат не соответствует ожидаемому";
+}
