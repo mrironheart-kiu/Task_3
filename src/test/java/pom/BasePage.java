932ccfc6e0db1bc07fba624d6b7bc2616b073d79
Kostin Ivan <mrironheart@gmail.com>
2026-03-12 22:35:21 +0300

    Добавил класс базовой страницы

---
+++ src/test/java/pom/BasePage.java
@@ -0,0 +1,43 @@
+package pom;
+
+import lombok.AllArgsConstructor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Базовый класс для всех страниц сайта Stellar Burgers
+ */
+@AllArgsConstructor
+public class BasePage {
+    protected WebDriver driver;
+
+    /**
+     * Метод нажимает на кнопку на странице
+     *
+     * @param locator локатор типа Bу для идентификации веб-элемента
+     */
+    public void clickButton(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    /**
+     * Метод проверяет активность поля и очищает его
+     *
+     * @param locator локатор типа Bу для идентификации веб-элемента
+     */
+    private void checkAndClearField(By locator) {
+        driver.findElement(locator).isEnabled();
+        driver.findElement(locator).clear();
+    }
+
+    /**
+     * Метод вводит в поле переданное значение
+     *
+     * @param locator  локатор типа Bу для идентификации веб-элемента
+     * @param newValue значение типа String для ввода в поле
+     */
+    public void setField(By locator, String newValue) {
+        checkAndClearField(locator);
+        driver.findElement(locator).sendKeys(newValue);
+    }
+}
