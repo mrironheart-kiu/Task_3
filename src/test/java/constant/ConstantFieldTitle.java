ba139ba4b3bf020e541d1a41d6393d013090e88c
Kostin Ivan <mrironheart@gmail.com>
2026-03-12 22:37:14 +0300

    Добавил константы наименования полей

---
+++ src/test/java/constant/ConstantFieldTitle.java
@@ -0,0 +1,7 @@
+package constant;
+
+public class ConstantFieldTitle {
+    public static final String BUTTON_TITLE_NAME = "Имя";
+    public static final String BUTTON_TITLE_EMAIL = "Email";
+    public static final String BUTTON_TITLE_PASSWORD = "Пароль";
+}
