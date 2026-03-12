abe4b3cc1f8a78c153efba9cbcb5ca4c5f7e04e0
Kostin Ivan <mrironheart@gmail.com>
2026-03-12 22:37:40 +0300

    Добавил константы путей для страниц сайта

--- src/test/java/constant/ConstantUrl.java
+++ src/test/java/constant/ConstantUrl.java
@@ -4,6 +4,7 @@ public class ConstantUrl {
     public static final String URL_MAIN_PAGE = "https://stellarburgers.education-services.ru";
     public static final String URL_REGISTER_PAGE = URL_MAIN_PAGE + "/register";
     public static final String URL_LOGIN_PAGE = URL_MAIN_PAGE + "/login";
+    public static final String URL_FORGOT_PASSWORD_PAGE = URL_MAIN_PAGE + "/forgot-password";
 
     public static final String URL_BASE_API_PATH = "/api";
     public static final String URL_BASE_API_AUTH_PATH = URL_BASE_API_PATH + "/auth";
