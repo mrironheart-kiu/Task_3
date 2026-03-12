ddce8117df58a934b6f89bf6f95f734043ec3397
Kostin Ivan <mrironheart@gmail.com>
2026-03-12 22:36:59 +0300

    Добавил константы наименования кнопок

---
+++ src/test/java/constant/ConstantButtonTitle.java
@@ -0,0 +1,8 @@
+package constant;
+
+public class ConstantButtonTitle {
+    public static final String BUTTON_TITLE_LOGIN = "Войти в аккаунт";
+    public static final String BUTTON_TITLE_LOGIN_SHORT = "Войти";
+    public static final String BUTTON_TITLE_ACCOUNT = "Личный кабинет";
+    public static final String BUTTON_TITLE_REGISTER = "Зарегистрироваться";
+}
