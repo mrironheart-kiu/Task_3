1a0799d75811f09ee4ff15e8643649eeb4e80cb2
Kostin Ivan <mrironheart@gmail.com>
2026-03-13 21:33:45 +0300

    Добавил Allure отчёт

--- src/test/java/basetest/BaseTest.java
+++ src/test/java/basetest/BaseTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import testdata.TestDataUser;
 
-import static constant.ConstantBrowser.CHROME;
+import static constant.ConstantBrowser.*;
 
 /**
  * Базовый класс для тестов
